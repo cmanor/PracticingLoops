8030740827f4d51f6b93ac5a0480d3b5b187d305
cmanor <101116379+cmanor@users.noreply.github.com>
2022-05-05 12:04:04 -0400

    all done

--- src/main/java/org/example/CountToEleven.java
+++ src/main/java/org/example/CountToEleven.java
@@ -1,7 +1,15 @@
 package org.example;
 
-public class CountToEleven {
-    public static void main(String[] args){
+import java.util.Scanner;
 
+public class CountToEleven {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a number less than 11");
+        int input = Integer.parseInt(scanner.nextLine());
+        for ( int i = input ; i <= 11; i++) {
+            System.out.println(i);
+        }
     }
 }
