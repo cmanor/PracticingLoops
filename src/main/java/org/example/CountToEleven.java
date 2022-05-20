292b7e476e2b924df4d12cdb079aa5905347a490
cmanor <101116379+cmanor@users.noreply.github.com>
2022-05-20 08:51:19 -0400

    making methods so I can test

--- src/main/java/org/example/CountToEleven.java
+++ src/main/java/org/example/CountToEleven.java
@@ -4,11 +4,13 @@ import java.util.Scanner;
 
 public class CountToEleven {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter a number less than 11");
+
         int input = Integer.parseInt(scanner.nextLine());
-        for ( int i = input ; i <= 11; i++) {
+        for (int i = input ; i <= 11; i++) {
             System.out.println(i);
         }
     }
--- src/main/java/org/example/Multiplication.java
+++ src/main/java/org/example/Multiplication.java
@@ -4,12 +4,16 @@ import java.util.Scanner;
 
 public class Multiplication {
     public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Input a number: ");
-        int input = Integer.parseInt(scanner.nextLine());
-        for (int i = 1; i<=10; i++) {
-            System.out.println(input + " x " + i + " = " + (input*i));
-        }
 
+        public void mTable() {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Input a number: ");
+            int input = Integer.parseInt(scanner.nextLine());
+
+            for (int i = 1; i <= 10; i++) {
+                System.out.println(input + " x " + i + " = " + (input * i));
+            }
+        }
     }
 }
